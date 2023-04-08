2dac77ca6258bda2878ddd4fd45f9d21049a498d
Jack Merma <jmermap@unsa.edu.pe>
2023-04-08 17:57:34 -0500

    independizando las vistas

--- gamesProject/urls.py
+++ gamesProject/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from homePage.views import homeView
+from django.urls import path, include
 
 urlpatterns = [
-    path('', homeView, name='Pagina de inicio'),
+    path('', include('homePage.urls')),
     path('admin/', admin.site.urls),
 ]
