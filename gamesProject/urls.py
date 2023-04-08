3aa2a08b9904c6b3a8c7fdd8e31ca3bfaed4df60
Jack Merma <jmermap@unsa.edu.pe>
2023-04-08 17:20:43 -0500

    agregando url de views de homePage

--- gamesProject/urls.py
+++ gamesProject/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for gamesProject project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
@@ -16,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from homePage.views import homeView
 
 urlpatterns = [
+    path('', homeView, name='Pagina de inicio'),
     path('admin/', admin.site.urls),
 ]
