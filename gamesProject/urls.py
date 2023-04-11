aabb8f85cc240bb7a56cc88eb911cd708aa171b9
dneirac <dneirac@unsa.edu.pe>
2023-04-10 20:03:23 -0500

    cambiando urls de accounts y no usando homepage

--- gamesProject/urls.py
+++ gamesProject/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', include('homePage.urls')),
-    path('account/', include('account.urls')),
+    #path('', include('homePage.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
+    #path('account/', include('account.urls')),
+    path('account/', include('allauth.urls')),
     path('admin/', admin.site.urls),
 ]
