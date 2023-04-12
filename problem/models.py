0d10cd08a7c10971a046a5e6dce1428ea9c9f4e3
dneirac <dneirac@unsa.edu.pe>
2023-04-12 18:08:01 -0500

    modificando modelo problem

--- problem/models.py
+++ problem/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 class Problem(models.Model):
-    userName = models.CharField(max_length = 20)
+    nameProblem = models.CharField(max_length = 20) 
     description = models.TextField()
     difficulty = models.IntegerField()
-    solved = models.BooleanField(default = False)
     hint = models.TextField()
 
