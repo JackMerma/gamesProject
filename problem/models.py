9a92421e6347dfce71f941a3bb5339fe61fe9d0b
dneirac <dneirac@unsa.edu.pe>
2023-04-12 17:11:11 -0500

    agregando userName

--- problem/models.py
+++ problem/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 class Problem(models.Model):
+    userName = models.CharField(max_length = 20)
     description = models.TextField()
-    hint = models.TextField()
     difficulty = models.IntegerField()
     solved = models.BooleanField(default = False)
+    hint = models.TextField()
+
