dd2c4c05488944d8a8ca60bd49b2f6e795b52106
Jack Merma <jmermap@unsa.edu.pe>
2023-04-13 20:26:54 -0500

    agregando url de cada objeto independiente

--- problem/models.py
+++ problem/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 class Problem(models.Model):
-    nameProblem = models.CharField(max_length = 20) 
+    nameProblem = models.CharField(max_length = 20)
     description = models.TextField()
     difficulty = models.IntegerField()
     hint = models.TextField()
 
+    def get_absolute_url(self):
+        return reverse('browsing', kwargs = {'myId' : self.id})
