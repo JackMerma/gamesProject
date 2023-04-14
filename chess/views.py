fbb502b2b35c628a948538221c1255d58eec6e5b
Jack Merma <jmermap@unsa.edu.pe>
2023-04-13 20:28:10 -0500

    vista para listado o problemset y vista para objetos independientes

--- chess/views.py
+++ chess/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import ChessProblem
 
 def chessView(request,*args, **kargs):
     return render(request, 'chess.html')
 
 def chessListView(request,*args, **kargs):
-    return render(request, 'chess.html')
+    queryset = ChessProblem.objects.all()
+    context = {
+        'objectList': queryset,
+    }
+    return render(request, 'chessProblemset.html', context)
+
+def chessShowObjectView(request, myId):
+    obj = get_object_or_404(ChessProblem, id = myId)
+    context = {
+        'object': obj,
+    }
+    return render(request, 'chessProblemDescription.html', context)
 
