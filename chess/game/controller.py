fa451296d1eb227b2d38f87cd6f14edf76083c75
Jack Merma <jmermap@unsa.edu.pe>
2023-04-15 19:57:10 -0500

    documentando funciones en controller

--- chess/game/controller.py
+++ chess/game/controller.py
@@ -2,20 +2,32 @@ from .interpreter import draw
 from .chessPictures import *
 import sys, traceback, re
 
+# funcion que valida que NO hayan algunas sentenicas aceptadas como:
+#   print()
+#   import
+#   from ... import
 def validate(code):
     for line in code.splitlines():
+        # uso de expresiones regulares para identificar casos independientes
         if re.search("^from.*import.*", line) or re.search("^import", line):
             return [True, "You can't use import statement"]
         if re.search("^print.*", line):
             return [True, "You can't use print statement"]
     return [False, ""]
 
+# Funcion que añade dos argumentos a la verdadera funcion draw
+# esto se hace para poder guardar la data generada al correr el codigo
+#
+# En especifico está diseñado para guardar el codigo, la imagen generada
+# y otros datos que se procesaran
 def putDataArguments(code, data, oldCode):
     output = ''
     for line in code.splitlines():
+        # expresion regular para identificar la linea de draw()
         result = re.search("(.*)draw\((.*)\)", line)
 
         if result:
+            # se le añaden los demas parametros
             output += result.group(1) + "draw([" + result.group(2) + ", '" + data[0] + "', '" + data[1] + "', '''" + oldCode + "'''])"
         else:
             output += line
@@ -24,17 +36,16 @@ def putDataArguments(code, data, oldCode):
 
 # retorna un booleano (error) y el error
 def create(code, data):
-    # statements
+    # validacion
     exp = validate(code)
     if exp[0]:
-        return exp[1] #retornando el error
+        return exp[1] # retornando el error
 
-    # running code
+    # corre el codigo con 'exec'
     try:
-        oldCode = code
+        oldCode = code # necesario para guardar la codigo original de cada usuario
         code = putDataArguments(code, data, oldCode)
-        print(code)
-        exec(code)
+        exec(code) # de lo demás se encargara la misma funcion draw()
         return ""
     except SyntaxError as err:
         error_class = err.__class__.__name__
