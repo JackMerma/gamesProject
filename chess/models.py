319b9351eccbabc66f11a53ada5e1dd47968a6ce
dneirac <dneirac@unsa.edu.pe>
2023-04-18 16:31:05 -0500

    agregando match a solution, y ubicacion de las imagenes de problems

--- chess/models.py
+++ chess/models.py
@@ -3,10 +3,11 @@ from problem.models import Problem
 from solution.models import Solution
 
 class ChessProblem(Problem):
-    imageChessProblem = models.ImageField(null=True, blank=True)
+    imageChessProblem = models.ImageField(null=True, blank=True, upload_to='images/chessProblems/')
     fileChessProblem = models.FileField(null=True, blank=True)
 
 class ChessSolution(Solution):
     codeChessSolution = models.TextField()
     imageChessSolution = models.ImageField(null=True, blank=True, upload_to='images/chessSolutions/')
     fileChessSolution = models.FileField(null=True, blank=True)
+    matchingChessSolution = models.DecimalField(max_digits=5,decimal_places=2, null=True, blank=True)
