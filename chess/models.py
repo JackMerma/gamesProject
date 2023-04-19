4920d35a17a207aec4b11534ad0c45181f7baa05
dneirac <dneirac@unsa.edu.pe>
2023-04-18 20:31:21 -0500

    modificacion de carpeta de files

--- chess/models.py
+++ chess/models.py
@@ -4,10 +4,10 @@ from solution.models import Solution
 
 class ChessProblem(Problem):
     imageChessProblem = models.ImageField(null=True, blank=True, upload_to='images/chessProblems/')
-    fileChessProblem = models.FileField(null=True, blank=True)
+    fileChessProblem = models.FileField(null=True, blank=True, upload_to='texts/chessProblems/')
 
 class ChessSolution(Solution):
     codeChessSolution = models.TextField()
     imageChessSolution = models.ImageField(null=True, blank=True, upload_to='images/chessSolutions/')
-    fileChessSolution = models.FileField(null=True, blank=True)
+    fileChessSolution = models.FileField(null=True, blank=True, upload_to='texts/chessSolutions/')
     matchingChessSolution = models.DecimalField(max_digits=5,decimal_places=2, null=True, blank=True)
