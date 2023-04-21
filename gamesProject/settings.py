bfb83da29781bdbeafa2c7f55b6e15cdf69f46ae
Jack Merma <jmermap@unsa.edu.pe>
2023-04-20 19:14:54 -0500

    agregando carpeta static en settings y .css

--- gamesProject/settings.py
+++ gamesProject/settings.py
@@ -132,6 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'chess_static'),
     os.path.join(BASE_DIR, 'homePage_static'),
+    os.path.join(BASE_DIR, 'account_static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
